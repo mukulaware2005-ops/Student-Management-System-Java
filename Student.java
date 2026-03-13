b88cb2cec594446efc0eec04bd5fa8f650832e1f
Mukul Aware <mukulaware2005@gmail.com>
2026-03-13 23:22:20 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,49 @@
+/*
+ public class Student {
+
+    int id;
+    String name;
+    int age;
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+}
+*/
+
+
+public class Student {
+
+    private final int id;
+    private String name;
+    private int age;
+
+    public Student(int id, String name, int age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    // Getters
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    // Setters
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
